c77f754711cb0d8f92222f6e3e78679b53896abf
SUBHAM PRASAD PANDA <79370554+Subham1901@users.noreply.github.com>
2021-03-17 18:07:08 +0530

    Add files via upload

---
+++ LogarithmOfNumber.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class LogarithmOfNumber {
+    public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+        double a=sc.nextDouble();
+			System.out.println("log(a) of "+a+" is: " +Math.log(a));
+            System.out.println("log10(a) of "+a+" is: " +Math.log10(a));
+		sc.close();
+    }
+    
+}
